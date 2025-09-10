2ea845beed60cfd565ffa687e2bf043a6035107e
jaydeepkale-21 <kalejaydeep21@email.com>
2025-09-11 00:21:06 +0530

    nested loop

--- dsa.java
+++ dsa.java
@@ -1,5 +1,22 @@
+
+import java.util.Scanner;
+
 public class dsa{
     public static void main(String[] args) {
-        System.out.println("hello");
+        int marks;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter your marks:");
+        marks=sc.nextInt();
+        if(marks>=35){
+            if(marks>=70){
+                System.out.println("you pass and give you grade 'A'");
+            }
+            else{
+                System.out.println("you only pass give grade 'B'");
+            }
+        }
+        else{
+            System.out.println("fail and give garde 'F'");
+        }
     }
 }
