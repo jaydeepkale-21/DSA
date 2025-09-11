0db064064127b9102ae9c55ea54a30dfe7dc8d7c
jaydeepkale-21 <kalejaydeep21@email.com>
2025-09-11 16:37:10 +0530

    question on contional statement

--- dsa.java
+++ dsa.java
@@ -1,22 +1,59 @@
 
 import java.util.Scanner;
 
+
 public class dsa{
     public static void main(String[] args) {
-        int marks;
-        Scanner sc=new Scanner(System.in);
-        System.out.println("enter your marks:");
-        marks=sc.nextInt();
-        if(marks>=35){
-            if(marks>=70){
-                System.out.println("you pass and give you grade 'A'");
-            }
-            else{
-                System.out.println("you only pass give grade 'B'");
-            }
-        }
-        else{
-            System.out.println("fail and give garde 'F'");
-        }
+        // --------switch case-----------
+    //  int days;
+    //  Scanner sc=new Scanner(System.in);
+    //  System.out.println("enter your day");
+    //  days=sc.nextInt();
+    //  switch (days) {
+    //      case 1:
+    //          System.out.println("monday");
+    //          break;
+    //     case 2:
+    //     System.out.println("tuesday");
+    //     break;
+    //     case 3:
+    //     System.out.println("wednessday");
+    //     break;
+    //     case 4:
+    //     System.out.println("thursday");
+    //     break;  
+    //     case 5:
+    //     System.out.println("friday");
+    //     break;
+    //     case 6:
+    //     System.out.println("saturday");
+    //     break;
+    //     case 7:
+    //     System.out.println("sunday");
+    //     break;     
+    //      default:
+    //         System.out.println("invaild");
+    
+
+        // -------------ternary opertor----------
+        // int a=63,b=36;
+        // boolean x= (a<b)?true:false;
+        // int y= (a>b)?a:b;
+        // System.out.println(x+ " " +y);
+
+        // ------------leap year--------------- 
+        // int year;
+        // Scanner sc=new Scanner(System.in);
+        // System.out.println("enter year to checking lep or not:");
+        // year=sc.nextInt();
+        // if(year<999){
+        //     System.out.println("invaild year enter check year!!");
+        // }
+        // else if(year%4==0){
+        //     System.out.println("this year is leap year: " + year);
+        // }
+        // else{
+        //     System.out.println("this normal year:" + year);
+        // }
+     }
     }
-}
