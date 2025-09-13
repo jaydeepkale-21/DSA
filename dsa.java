3e179a8b0b3ea558d591e3132351d89c50e73cc0
jaydeepkale-21 <kalejaydeep21@email.com>
2025-09-13 12:42:12 +0530

    while loop

--- dsa.java
+++ dsa.java
@@ -4,56 +4,38 @@ import java.util.Scanner;
 
 public class dsa{
     public static void main(String[] args) {
-        // --------switch case-----------
-    //  int days;
-    //  Scanner sc=new Scanner(System.in);
-    //  System.out.println("enter your day");
-    //  days=sc.nextInt();
-    //  switch (days) {
-    //      case 1:
-    //          System.out.println("monday");
-    //          break;
-    //     case 2:
-    //     System.out.println("tuesday");
-    //     break;
-    //     case 3:
-    //     System.out.println("wednessday");
-    //     break;
-    //     case 4:
-    //     System.out.println("thursday");
-    //     break;  
-    //     case 5:
-    //     System.out.println("friday");
-    //     break;
-    //     case 6:
-    //     System.out.println("saturday");
-    //     break;
-    //     case 7:
-    //     System.out.println("sunday");
-    //     break;     
-    //      default:
-    //         System.out.println("invaild");
+//   loop-> Loop are use to perform repetaed task
+// -----------while loop--------
+// int n;
+// Scanner sc=new Scanner(System.in);
+// System.out.println("enter number to give count:");
+// n=sc.nextInt();
+// int num=1;
+// int sum=0;
+// while(num<=n){
+//     System.out.print(num+" ");
+//     sum+=num;
+//     num++;
+// }
+// System.out.println("sum on n natural number:"+sum);
+
+
+// -------------for loop------
+// int n=10899;
+int reverse=0;
+// for(int i=1;i<6;i++){
+//     reverse=n%10;
+//     n/=10;
+//       System.out.print(reverse);
+// }
+int n=10899;
+while(n>0){
+    reverse=n%10;
+    System.out.print(reverse);
+    n/=10;
+  
+}
     
 
-        // -------------ternary opertor----------
-        // int a=63,b=36;
-        // boolean x= (a<b)?true:false;
-        // int y= (a>b)?a:b;
-        // System.out.println(x+ " " +y);
-
-        // ------------leap year--------------- 
-        // int year;
-        // Scanner sc=new Scanner(System.in);
-        // System.out.println("enter year to checking lep or not:");
-        // year=sc.nextInt();
-        // if(year<999){
-        //     System.out.println("invaild year enter check year!!");
-        // }
-        // else if(year%4==0){
-        //     System.out.println("this year is leap year: " + year);
-        // }
-        // else{
-        //     System.out.println("this normal year:" + year);
-        // }
      }
     }
