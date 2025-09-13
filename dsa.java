5b55da06bdee104bd12f0526badb4f66d27206da
jaydeepkale-21 <kalejaydeep21@email.com>
2025-09-14 00:18:01 +0530

    patten printing

--- dsa.java
+++ dsa.java
@@ -1,41 +1,84 @@
-
-import java.util.Scanner;
-
-
 public class dsa{
     public static void main(String[] args) {
-//   loop-> Loop are use to perform repetaed task
-// -----------while loop--------
+
+        //----------Reverse number ------------ 
+
+        // int num;
+        // Scanner sc=new Scanner(System.in);
+        // System.out.println("enter any number that give reverse of them:");
+        // num=sc.nextInt();
+        // int rev=0;
+        // while(num>0){
+        //     int last_digit=num%10;
+        //     rev=(rev*10)+last_digit;
+        //     num/=10;
+        // }
+        // System.out.println(rev);
+
+
+        // --------Example on do while loop----------
+    //      int num;
+    //     Scanner sc=new Scanner(System.in);
+    //     System.out.println("enter you number:");
+    //     do { 
+    //          num=sc.nextInt();
+    //          if(num%10==0){
+    //             System.out.println("skip number");
+    //         continue;
+    //          }
+    //          System.out.println("displaying number:"+num);
+    //     } while (num<100);
+
+    // int num;
+    // Scanner sc=new Scanner(System.in);
+    // System.out.println("enter you number:");
+    //  num=sc.nextInt();
+    //  boolean isprime=true;
+    //  if(num==2){
+    //     System.out.println("it is prime number");
+    //  }
+    //  else{
+    //      for(int i=2;i<=(Math.sqrt(num));i++){
+    //     if(num%i==0){
+    //         isprime=false;
+    //     }
+    //  }
+    //  if(isprime==true){
+    //     System.out.println("it is an prime number:");
+    //  }
+    //  else{
+    //     System.out.println("it not prime number");
+    //  }
+    //  }
 // int n;
 // Scanner sc=new Scanner(System.in);
-// System.out.println("enter number to give count:");
 // n=sc.nextInt();
-// int num=1;
-// int sum=0;
-// while(num<=n){
-//     System.out.print(num+" ");
-//     sum+=num;
-//     num++;
+// for(int i=1;i<=n;i++){
+//     for(int j=1;j<=i;j++){
+//         System.out.print(j);
+//     }
+//     System.out.println();
 // }
-// System.out.println("sum on n natural number:"+sum);
-
-
-// -------------for loop------
-// int n=10899;
-int reverse=0;
-// for(int i=1;i<6;i++){
-//     reverse=n%10;
-//     n/=10;
-//       System.out.print(reverse);
-// }
-int n=10899;
-while(n>0){
-    reverse=n%10;
-    System.out.print(reverse);
-    n/=10;
-  
-}
+// char ch=(int)(64+1);
+// System.out.print(ch);
+// ch=(int)(64+2);
+// System.out.println(ch);
+// int c=(char)ch;
+// System.out.println(c);
+// char ch='A';
+// for(int i=1;i<=5;i++){
     
+//     for(int j=1;j<=i;j++){
+//         System.out.print(ch);
+//          ch++;
+//     }
+//     System.out.println();
+// }
 
-     }
+
+
+
+
     }
+
+}
