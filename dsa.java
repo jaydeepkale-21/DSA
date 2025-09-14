37034519769325b9ad9668456b2997258cf16ef8
jaydeepkale-21 <kalejaydeep21@email.com>
2025-09-15 00:03:23 +0530

    function /method

--- dsa.java
+++ dsa.java
@@ -1,84 +1,19 @@
+import java.security.PublicKey;
+import java.util.Scanner;
 public class dsa{
+    //function /method
+    public static int sum(int a,int b){
+            int sum=a+b;
+            return sum;
+        }
     public static void main(String[] args) {
-
-        //----------Reverse number ------------ 
-
-        // int num;
-        // Scanner sc=new Scanner(System.in);
-        // System.out.println("enter any number that give reverse of them:");
-        // num=sc.nextInt();
-        // int rev=0;
-        // while(num>0){
-        //     int last_digit=num%10;
-        //     rev=(rev*10)+last_digit;
-        //     num/=10;
-        // }
-        // System.out.println(rev);
-
-
-        // --------Example on do while loop----------
-    //      int num;
-    //     Scanner sc=new Scanner(System.in);
-    //     System.out.println("enter you number:");
-    //     do { 
-    //          num=sc.nextInt();
-    //          if(num%10==0){
-    //             System.out.println("skip number");
-    //         continue;
-    //          }
-    //          System.out.println("displaying number:"+num);
-    //     } while (num<100);
-
-    // int num;
-    // Scanner sc=new Scanner(System.in);
-    // System.out.println("enter you number:");
-    //  num=sc.nextInt();
-    //  boolean isprime=true;
-    //  if(num==2){
-    //     System.out.println("it is prime number");
-    //  }
-    //  else{
-    //      for(int i=2;i<=(Math.sqrt(num));i++){
-    //     if(num%i==0){
-    //         isprime=false;
-    //     }
-    //  }
-    //  if(isprime==true){
-    //     System.out.println("it is an prime number:");
-    //  }
-    //  else{
-    //     System.out.println("it not prime number");
-    //  }
-    //  }
-// int n;
-// Scanner sc=new Scanner(System.in);
-// n=sc.nextInt();
-// for(int i=1;i<=n;i++){
-//     for(int j=1;j<=i;j++){
-//         System.out.print(j);
-//     }
-//     System.out.println();
-// }
-// char ch=(int)(64+1);
-// System.out.print(ch);
-// ch=(int)(64+2);
-// System.out.println(ch);
-// int c=(char)ch;
-// System.out.println(c);
-// char ch='A';
-// for(int i=1;i<=5;i++){
-    
-//     for(int j=1;j<=i;j++){
-//         System.out.print(ch);
-//          ch++;
-//     }
-//     System.out.println();
-// }
-
-
-
-
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    int addtion =sum(a, b);
+    System.out.println("final addtion is:"+addtion);
+        }
 
     }
 
-}
+
