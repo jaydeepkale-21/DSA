b299e0156e30f733d73f64df378dba7eac2de066
jaydeepkale-21 <kalejaydeep21@email.com>
2025-09-18 22:42:24 +0530

    convertion decimal and binary

--- dsa.java
+++ dsa.java
@@ -1,19 +1,44 @@
-import java.security.PublicKey;
-import java.util.Scanner;
+import java.util.*;
 public class dsa{
-    //function /method
-    public static int sum(int a,int b){
-            int sum=a+b;
-            return sum;
-        }
-    public static void main(String[] args) {
+public static void main(String[] args) {
     Scanner sc=new Scanner(System.in);
-    int a=sc.nextInt();
-    int b=sc.nextInt();
-    int addtion =sum(a, b);
-    System.out.println("final addtion is:"+addtion);
-        }
+    System.out.println("Enter a binary number:");
+  int num=sc.nextInt();
+  decimalTObinary(num);
+}
 
+// ---------binary to decimal
+
+//   public static void binTOdecimal(int num){
+//     int decimal=0;
+//     int i=0;
+//   while(num>0){
+//     int last_digit=num%10;
+//     decimal=decimal+(last_digit*(int)Math.pow(2,i));
+//     num/=10;
+//     i++;
+//   }
+//   System.out.println(decimal);
+//   }
+
+// ------------decimal to binary-----------
+public static void decimalTObinary(int num){
+    int pow=0;
+    int binNum=0;
+    while(num>0){
+        int rem=num%2;
+       binNum=binNum+(rem*(int)Math.pow(10,pow));
+       pow++;
+       num/=2;
     }
+    System.out.println("binary number is:"+binNum);
+    }
+  }
+
+
+
+
+
+
 
 
