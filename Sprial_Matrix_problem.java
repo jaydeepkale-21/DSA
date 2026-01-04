b54b8cade586c0eb31a7d452e78a722d6f09e28f
jaydeepkale-21 <kalejaydeep21@email.com>
2026-01-04 09:56:06 +0530

    2D array & Problem solve

---
+++ Sprial_Matrix_problem.java
@@ -0,0 +1,48 @@
+public class Sprial_Matrix_problem {
+       public static void Print_Sprial(int arr[][]){
+       int start_row=0;
+       int end_row=arr.length-1;
+       int start_col=0;
+       int end_col=arr[0].length-1;
+       while(start_row<=end_row && start_col<=end_col){
+        // Top
+        for(int j=start_col;j<=end_col;j++){
+            System.out.print(arr[start_col][j]+" ");
+        }
+        // Right
+        for(int i=start_row+1;i<=end_col;i++){
+            System.out.print(arr[i][end_col]+" ");
+        }
+        // Bottam
+        for(int j=end_col-1;j>=start_col;j--){
+            if(start_row==end_row){
+                break;
+            }
+            System.out.print(arr[end_row][j]+" ");
+        }
+        // left
+        for(int i=end_row-1;i>=start_col;i--){
+            if(start_col==end_col){
+                break;
+            }
+            System.out.print(arr[i][start_col]+" ");
+        }
+        start_col++;
+        start_row++;
+        end_col--;
+        end_row--;
+       }
+       System.out.println();
+        }
+
+        public static void main(String[] args) {
+            int arr[][]={
+                {1,2,3,4},
+                {5,6,7,8},
+                {9,10,11,12},
+                {13,14,15,16}
+            };
+            Print_Sprial(arr);
+        }
+        
+    }
---
+++ TwoDArray.java
@@ -0,0 +1,46 @@
+
+import java.util.Scanner;
+
+public class TwoDArray {
+    public static void Find_Max_Min(int arr[][]){
+        int max=Integer.MIN_VALUE;
+        int min=Integer.MAX_VALUE;
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[i].length;j++){
+                max=Math.max(max, arr[i][j]);
+                min=Math.min(min, arr[i][j]);
+            }
+        }
+        System.out.println("Maximum number is: "+max);
+        System.out.println("Minimum number is: "+min);
+    }
+
+    // Printing 
+      public static void Print_Sprial(int arr[][]){
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[i].length;j++){
+               if(i,j==1 ){
+
+               }
+            }
+        }
+        
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter row Size: ");
+        int row=sc.nextInt();
+        System.out.println("Enter col size: ");
+        int col=sc.nextInt();
+        int arr[][]=new int[row][col];
+        System.out.println("Enter Element: ");
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[i].length;j++){
+                arr[i][j]=sc.nextInt();
+            }
+        }
+
+        Find_Max_Min(arr);
+
+    }
+}
